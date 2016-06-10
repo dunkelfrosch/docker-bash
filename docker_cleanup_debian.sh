9bd7ee817aee6f922144b504010929132df46d2b
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2016-06-10 15:36:05 +0200

    minor changes in docker container detection and inline documentation

--- docker_cleanup_debian.sh
+++ docker_cleanup_debian.sh
@@ -3,19 +3,19 @@
 # simple self destructive cleanUp script for debian/ubuntu OS based docker container build process
 #
 # @copyright (c) 2015-2016 dunkelfrosch, patrick.paechnatz@gmail.com,
-# @version 1.0.0
+# @version 1.0.1
 #
 # setup/usage:
 #
 # 1.) transfer this script using ADD directive inside your Dockerfile:
 #
-#    ADD
+#    ADD https://raw.githubusercontent.com/dunkelfrosch/docker-bash/master/docker_cleanup_debian.sh /tmp
 #
 # 2.) execute this script inside your container during package-manager related build process has finished:
 #
-#    RUN set -e sh /opt/docker/cleanup.sh
+#    RUN set -e sh /opt/docker/docker_cleanup_debian.sh
 #
-if [ -f /.dockerinit ]; then
+if [ -f /proc/1/cgroup ]; then
     apt-get clean autoclean >/dev/null 2>&1 && \
     apt-get autoremove -y >/dev/null 2>&1
     rm -rf /var/lib/cache /var/lib/log /tmp/* /var/tmp/*
@@ -30,8 +30,7 @@ if [ -f /.dockerinit ]; then
     echo '' > /var/log/lastlog && \
     echo '' > /var/log/bootstrap.log && \
     echo '' > /var/log/alternatives.log && \
-    rm -f /opt/docker/cleanup.sh
-
+    rm -f /tmp/docker_cleanup_debian.sh
 else
     echo -e "\033[0;31m this script can only be executed inside container environment <EXIT>!\033[0m";
     exit 127
